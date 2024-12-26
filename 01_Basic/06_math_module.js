81e40dbed3531961d8fcfc5cabe2d1228e55bbcb
Akhilesh Thapliyal <thapliyalakhilesh29@gmail.com>
2024-12-26 10:42:59 +0530

    new file added - objcet.js

--- 01_Basic/06_math_module.js
+++ 01_Basic/06_math_module.js
@@ -1,4 +1,4 @@
-num = Math.random()
+num = (Math.random()+10)
 console.log(num)
 if (num+1 == 2) {
     console.log("You Win")
---
+++ 01_Basic/08_object.js
@@ -0,0 +1,5 @@
+//There are twi ways to declare objcets
+const obj1 = {}
+const obj2 = new Object()
+console.log(obj1);
+console.log(obj2);
