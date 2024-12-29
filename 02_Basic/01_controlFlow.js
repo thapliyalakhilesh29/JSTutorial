619a955e52beef41a42dfb83f802a4ff00e9eaa9
Akhilesh Thapliyal <thapliyalakhilesh29@gmail.com>
2024-12-29 20:43:40 +0530

    ternary operator functionality

--- 02_Basic/01_controlFlow.js
+++ 02_Basic/01_controlFlow.js
@@ -27,3 +27,32 @@
 
 
 // Switch case---- used when multiple if else atatement is there.
+// break is essential in switch statements to prevent fall-through behavior.
+//Without break, JavaScript will execute all cases after a matching condition, even if they don’t match.
+
+const fruit = 'apple';
+
+switch (fruit) {
+    case 'apple':
+        console.log('It’s an apple!');
+        break;
+    case 'banana':
+        console.log('It’s a banana!');
+        break;
+    default:
+        console.log('Unknown fruit');
+}
+//Without break: Both 'apple' and 'banana' cases would execute.
+
+
+/* 
+?: (Ternary Operator)
+The ternary operator (?:) is a conditional (if-else) operator.
+It evaluates a boolean condition and returns one of two values based on whether the condition is true or false.
+*/
+
+const score = 85;
+const grade = score >= 60 ? 'Pass' : 'Fail';
+
+console.log(grade); // Output: 'Pass'
+
