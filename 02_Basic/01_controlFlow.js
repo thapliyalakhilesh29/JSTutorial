130841105fe206aadae1919c962bfdcc6db641e1
Akhilesh Thapliyal <thapliyalakhilesh29@gmail.com>
2024-12-29 13:32:39 +0530

    control flow--if elese & switch case

---
+++ 02_Basic/01_controlFlow.js
@@ -0,0 +1,29 @@
+// if & if else statements............ 
+
+//const age = 59
+// if (age >= 60){
+//     console.log(`The person is senior citizen since his age is ${age}`);
+//     }
+
+
+// if (age<=18){
+//     console.log("The person is not eligible for voting");   
+// }
+// else{
+//     console.log("This person is eligible for voting"); 
+// }
+
+
+// if (age<= 18){
+//     console.log("The person is not eligible for voting");   
+// }
+// else if (age> 18 && age< 60){
+//     console.log("The person is Adult");
+//     }
+// else{
+//     console.log("The person is sinior citzen");
+    
+// }
+
+
+// Switch case---- used when multiple if else atatement is there.
